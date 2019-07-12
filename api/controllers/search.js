828f36975de5586532933a0f47d24cdfa49fc2d1
fitsumayalew <40556774+fitsumayalew@users.noreply.github.com>
2019-07-12 12:39:59 +0300

    implmented the search controller

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
+const Song = require("../models/song");
 
+
+exports.search = (req, res, next) => {
+    Song.find({ $text: { $search: req.body.searchString } } )
+        .select()
+        .populate("artist", "_id name")
+        .populate("album", "_id name albumImage")
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+}
