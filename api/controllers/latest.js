4d7c03589cea12b7843b41fe63cf28d519113cc5
fitsumayalew <40556774+fitsumayalew@users.noreply.github.com>
2019-07-12 12:39:11 +0300

    implmented the get latest controller

--- api/controllers/latest.js
+++ api/controllers/latest.js
@@ -1,2 +1,22 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
+
+const Album = require("../models/album");
+
+
+exports.get_latest = (req, res, next) => {
+        Album.find()
+            .sort([['uploadDate', 'descending']])
+            .populate("artist", "_id name")
+            .exec()
+            .then(result => {
+                if (result.length > 0) {
+                    res.status(200).json(result);
+                }
+            })
+            .catch (err => {
+    console.log(err);
+    res.status(500).json({
+        error: err
+    });
+});
+}
